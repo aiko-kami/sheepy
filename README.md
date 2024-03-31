4370a910df1fe4cf007de564d9e6c5bbea998651
Adrian-loup <adrianloup@gmail.com>
2024-03-31 21:00:45 +0200

    added mobile version for Hero

--- README.md
+++ README.md
@@ -1,38 +1,3 @@
-## Getting Started
-
-First, run the development server:
-
-```bash
-npm run dev
-# or
-yarn dev
-# or
-pnpm dev
-# or
-bun dev
-```
-
-Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
-
-You can start editing the page by modifying `app/page.js`. The page auto-updates as you edit the file.
-
-This project uses [`next/font`](https://nextjs.org/docs/basic-features/font-optimization) to automatically optimize and load Inter, a custom Google Font.
-
-## Learn More
-
-To learn more about Next.js, take a look at the following resources:
-
-- [Next.js Documentation](https://nextjs.org/docs) - learn about Next.js features and API.
-- [Learn Next.js](https://nextjs.org/learn) - an interactive Next.js tutorial.
-
-You can check out [the Next.js GitHub repository](https://github.com/vercel/next.js/) - your feedback and contributions are welcome!
-
-## Deploy on Vercel
-
-The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
-
-Check out our [Next.js deployment documentation](https://nextjs.org/docs/deployment) for more details.
-
 <div align="center">
 
 [![author - @aiko-kami](https://img.shields.io/badge/author-%40aiko--kami-blue)](https://www.github.com/aiko-kami)
@@ -40,9 +5,8 @@ Check out our [Next.js deployment documentation](https://nextjs.org/docs/deploym
 ![GitHub package.json version (branch)](https://img.shields.io/github/package-json/v/aiko-kami/panda-server/master)
 ![GitHub repo size](https://img.shields.io/github/repo-size/aiko-kami/panda-server)
 
-![Static Badge](https://img.shields.io/badge/-JavaScript-yellow?logo=nodedotjs&labelColor=gray)
-![Static Badge](https://img.shields.io/badge/-ExpressJS-yellow?logo=express&labelColor=gray)
-![Static Badge](https://img.shields.io/badge/-MongoDB-brightgreen?logo=mongodb&labelColor=grey)
+![Static Badge](https://img.shields.io/badge/-Next.js-black?logo=next.js&labelColor=gray)
+![Static Badge](https://img.shields.io/badge/-TailwindCSS-blue?logo=tailwindcss&labelColor=gray&color=%2306B6D4)
 
 </div>
 
@@ -129,11 +93,13 @@ Install the dependencies with npm:
   npm install
 ```
 
-Start the application in dev mode:
+Start the application in development mode:
 
 ```bash
   npm run dev
 ```
+
+Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
 
 ## 🗂 Code Structure
 
@@ -300,7 +266,7 @@ Please contact neutroneer100@gmail.com
 
 ## 🌮 Demo
 
-NextJS version: [Sheepy](https://sheepy.vercel.app/)
+[Sheepy](https://sheepy.vercel.app/)
 
 ## 🧗‍♂️ Status
 
--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -14,6 +14,51 @@ import satellite from "../../../public/assets/hero/satellite.png";
 const Hero = () => {
 	return (
 		<>
+			{/* Mobile version */}
+			<div className="lg:hidden test flex h-160 relative bg-gradient-to-b from-custom-blue-dark to-custom-blue-light">
+				<div className="bg-white w-1 h-1 rounded-full absolute top-28 left-30"></div>
+				<div className="bg-white w-1 h-1 rounded-full absolute bottom-28 left-32"></div>
+				<div className="bg-white w-1 h-1 rounded-full absolute bottom-72 left-80"></div>
+				<div className="bg-white w-1 h-1 rounded-full absolute top-38 left-1/3 -translate-x-48"></div>
+				<div className="bg-white w-1 h-1 rounded-full absolute top-60 left-1/3 -translate-x-22"></div>
+				<div className="bg-white w-1 h-1 rounded-full absolute top-84 left-1/3 -translate-x-20"></div>
+				<div className="bg-white w-1 h-1 rounded-full absolute bottom-66 left-1/3 -translate-x-14"></div>
+				<div className="bg-white w-1 h-1 rounded-full absolute bottom-48 left-1/3 -translate-x-28"></div>
+				<div className="bg-white w-1 h-1 rounded-full absolute bottom-22 left-1/3 translate-x-22"></div>
+				<div className="bg-white w-1 h-1 rounded-full absolute bottom-12 left-1/3 -translate-x-2"></div>
+				<div className="bg-white w-1 h-1 rounded-full absolute top-28 left-1/3"></div>
+				<div className="bg-white w-1 h-1 rounded-full absolute top-16 left-1/2 translate-x-20"></div>
+				<div className="bg-white w-1 h-1 rounded-full absolute top-38 left-3/5"></div>
+				<div className="bg-white w-1 h-1 rounded-full absolute top-46 left-1/2 translate-x-66"></div>
+				<div className="bg-white w-1 h-1 rounded-full absolute top-56 right-82"></div>
+				<div className="bg-white w-1 h-1 rounded-full absolute top-90 right-32"></div>
+				<div className="bg-white w-1 h-1 rounded-full absolute bottom-22 right-1/4 translate-x-38"></div>
+				<div className="bg-white w-1 h-1 rounded-full absolute bottom-14 right-1/4 translate-x-6"></div>
+				<div className="bg-white w-1 h-1 rounded-full absolute bottom-44 right-1/4 translate-x-2"></div>
+				<div className="bg-white w-1 h-1 rounded-full absolute bottom-54 right-1/4 -translate-x-20"></div>
+				<div className="bg-white w-1 h-1 rounded-full absolute bottom-86 right-1/4 -translate-x-26"></div>
+				<Image src={spaceship} width={70} alt="spaceship" className="absolute top-22 left-1/2 -translate-x-1/2" />
+				<Image src={planetBlue} width={60} alt="planetBlue" className="absolute top-6 left-6" />
+				<Image src={sun} width={90} alt="sun" className="absolute top-10 right-6 " />
+				<Image src={galaxy} width={40} alt="galaxy" className="absolute top-8 right-34" />
+				<Image src={comet} width={45} alt="comet" className="absolute top-32 left-14" />
+				<Image src={planetRed} width={60} alt="planetRed" className="absolute bottom-18 right-8" />
+				<Image src={saturn} width={50} alt="saturn" className="absolute bottom-10 left-8" />
+				<Image src={spaceman} width={60} alt="spaceman" className="absolute bottom-49 left-1/2" />
+				<Image src={satellite} width={45} alt="satellite" className="absolute bottom-56 left-8" />
+				<div className="absolute left-1/2 top-58 -translate-x-1/2 w-full">
+					<h2 className="text-center text-2xl font-oldenburg mb-6">Bring your ideas to life</h2>
+					<h2 className="text-center text-2xl font-oldenburg">by finding the best talents</h2>
+				</div>
+				<button
+					type="button"
+					className="text-white text-lg bg-green-500 hover:bg-green-600 active:bg-green-900 transition duration-300 font-medium rounded-xl px-5 py-2.5 text-center absolute left-1/2 -translate-x-1/2 bottom-38"
+				>
+					Start my project
+				</button>
+			</div>
+
+			{/* Large screen version */}
 			<div className="hidden lg:flex h-180 mb-8 relative bg-gradient-to-b from-custom-blue-dark to-custom-blue-light">
 				<div className="bg-white w-1 h-1 rounded-full absolute top-28 left-30"></div>
 				<div className="bg-white w-1 h-1 rounded-full absolute bottom-28 left-32"></div>
@@ -46,7 +91,7 @@ const Hero = () => {
 				<Image src={spaceman} width={75} alt="spaceman" className="absolute bottom-40 left-1/2 translate-x-8" />
 				<Image src={satellite} width={50} alt="satellite" className="absolute bottom-60 left-28" />
 				<div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
-					<h2 className="text-center text-4xl font-oldenburg">Bring your ideas to life</h2>
+					<h2 className="text-center text-4xl font-oldenburg mb-10">Bring your ideas to life</h2>
 					<h2 className="text-center text-4xl font-oldenburg">by finding the best talents</h2>
 				</div>
 				<button
