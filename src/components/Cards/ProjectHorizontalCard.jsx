c33d68844ad6c112de70f12f6dafedea45f8dffc
Adrian-loup <adrianloup@gmail.com>
2024-04-23 22:42:35 +0200

    Added links in projects list

--- src/components/Cards/ProjectHorizontalCard.jsx
+++ src/components/Cards/ProjectHorizontalCard.jsx
@@ -6,11 +6,15 @@ const ProjectHorizontalCard = ({ projectData }) => {
 
 	return (
 		<>
-			<div className="relative grid grid-cols-4 items-center mx-2 lg:mx-14 mb-3 test">
-				<Image src={projectCover} width={1280} height={1280} alt="Project picture" className="object-cover h-full shadow-md rounded-lg" />
+			<div className="relative grid grid-cols-4 items-center mx-2 lg:mx-14 mb-3">
+				<Link href="/projects/01">
+					<Image src={projectCover} width={1280} height={1280} alt="Project picture" className="object-cover h-full shadow-md rounded-lg" />
+				</Link>
 
 				<div className="ml-6 col-span-2">
-					<h3 className="font-semibold text-xl pb-1">{projectTitle}</h3>
+					<Link href="/projects/01">
+						<h3 className="font-semibold text-xl pb-1">{projectTitle}</h3>
+					</Link>
 					<p>{projectSummary}</p>
 				</div>
 			</div>
