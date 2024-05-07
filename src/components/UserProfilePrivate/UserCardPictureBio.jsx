194e50bc74b6f322a1a51574ab67a0bd0abf2422
aiko-kami <adrianloup@gmail.com>
2024-05-07 11:29:38 +0200

    fixed display flex of popover

--- src/components/UserProfilePrivate/UserCardPictureBio.jsx
+++ src/components/UserProfilePrivate/UserCardPictureBio.jsx
@@ -35,32 +35,32 @@ const UserCardPictureBio = ({ user }) => {
 							<Popover displayPopover={displayPopover}>
 								<ul className="py-1">
 									<li className="text-sm hover:bg-gray-200 duration-200 active:text-base-450 block p-2 pl-1">
-										<div className="flex">
-											<Link href="#">
+										<div>
+											<Link href="#" className="flex">
 												<IoCameraReverseOutline className="w-5 h-5 mr-1" />
 												Change profile picture
 											</Link>
 										</div>
 									</li>
 									<li className="text-sm hover:bg-gray-200 duration-200 active:text-base-450 block p-2 pl-1">
-										<div className="flex">
-											<Link href="#">
+										<div>
+											<Link href="#" className="flex">
 												<IoClose className="w-5 h-5 mr-1" />
 												Remove profile picture
 											</Link>
 										</div>
 									</li>
 									<li className="text-sm hover:bg-gray-200 duration-200 active:text-base-450 block p-2 pl-1">
-										<div className="flex">
-											<Link href="#">
+										<div>
+											<Link href="#" className="flex">
 												<IoImageOutline className="w-5 h-5 mr-1" />
 												Change background
 											</Link>
 										</div>
 									</li>
 									<li className="text-sm hover:bg-gray-200 duration-200 active:text-base-450 block p-2 pl-1">
-										<div className="flex">
-											<Link href="#">
+										<div>
+											<Link href="#" className="flex">
 												<IoClose className="w-5 h-5 mr-1" />
 												Remove background
 											</Link>
