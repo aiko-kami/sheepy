18a5f1c527d6d4a7e824b64070525f7a6aed9b7d
Adrian-loup <adrianloup@gmail.com>
2024-07-06 09:41:20 +0200

    started step one

--- src/components/StartProjectForm/Step1.jsx
+++ src/components/StartProjectForm/Step1.jsx
@@ -1,10 +1,37 @@
+"use client";
+
 import Link from "next/link";
+import { Button } from "@/components/Buttons/Buttons";
+import FormField from "@/components/Forms/FormField";
+
+import { IoAtOutline, IoLockClosed } from "react-icons/io5";
 
 const StepOne = () => {
+	// Handle form submission
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		// You can perform actions like searching here
+		console.log("Search string:", searchInput);
+	};
+
+	const onChange = () => {};
+
 	return (
 		<>
-			<div className="container mx-auto p-8 mt-12 mb-20 text-justify">
-				<p className="mb-4 mx-1/5 text-center">Content of step 1 </p>
+			<div className="container mx-auto p-8 mt-12 mb-20 text-justify test">
+				<p className="mb-4 text-center">Content of step 1 </p>
+				<form onSubmit={handleSubmit}>
+					{/* List of fields */}
+					<div className="mb-10">
+						{/* Project Title */}
+						<FormField inputName="projectTitle" inputType="text" label="Project title" inputValue=""></FormField>
+					</div>
+					{/* Button Update account (submit form) */}
+					<div className="text-center">
+						<Button btnProps={{ type: "submit" }}>Back to introduction</Button>
+						<Button btnProps={{ type: "submit" }}>Go to next step</Button>
+					</div>
+				</form>
 			</div>
 		</>
 	);
