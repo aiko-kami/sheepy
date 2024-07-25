e4321c5486f256f8744aa369b7f9379416c14e7d
Adrian-loup <adrianloup@gmail.com>
2024-07-25 23:41:35 +0200

    improved final step validation

--- src/components/StartProjectForm/StepFinalValidation.jsx
+++ src/components/StartProjectForm/StepFinalValidation.jsx
@@ -14,74 +14,74 @@ const StepFinalValidation = ({ formInputs, talentNeededProfilePicture }) => {
 
 	return (
 		<>
-			<div className="container min-w-full m-auto pr-2 lg:px-8 text-justify">
+			<div className="container min-w-full m-auto pr-2 sm:px-4 xl:px-30 text-justify">
 				<h1 className="text-4xl md:text-7xl pt-4 md:pt-6 mb-8 md:mb-20 text-center">Final step</h1>
 				<p className="mb-8">
 					Before you submit your project, please take a moment to review all the information you have provided. Carefully check each field for any errors or omissions. Make sure that your project
 					description is clear, your goals are well-defined, and all necessary details are included. This final review is crucial to ensure that your project has the best chance of success. Once
 					you're confident that everything is in order, you can proceed to submit your project. Good luck!
 				</p>
-				<div className="pl-6">
+				<div className="sm:px-6 xl:px-14">
 					<div className="mb-6">
 						<h2 className="text-xl font-bold mb-2">Project title</h2>
-						<p>{formInputs.projectTitle}</p>
+						<p className="pl-4 sm:pl-10 xl:pl-60">{formInputs.projectTitle}</p>
 					</div>
 					<div className="mb-6">
 						<h2 className="text-xl font-bold mb-2">Category</h2>
-						<p>{formInputs.selectedCategory}</p>
+						<p className="pl-4 sm:pl-10 xl:pl-60">{formInputs.selectedCategory}</p>
 					</div>
 					<div className="mb-6">
 						<h2 className="text-xl font-bold mb-2">Sub-category</h2>
-						<p>{formInputs.selectedSubCategory}</p>
+						<p className="pl-4 sm:pl-10 xl:pl-60">{formInputs.selectedSubCategory}</p>
 					</div>
 					<div className="mb-6">
 						<h2 className="text-xl font-bold mb-2">Project summary</h2>
-						<p>{formInputs.projectSummary}</p>
+						<p className="pl-4 sm:pl-10 xl:pl-60">{formInputs.projectSummary}</p>
 					</div>
 					<div className="mb-6">
 						<h2 className="text-xl font-bold mb-2">Project goal</h2>
-						<p>{formInputs.projectGoal}</p>
+						<p className="pl-4 sm:pl-10 xl:pl-60">{formInputs.projectGoal}</p>
 					</div>
 					<div className="mb-6">
 						<h2 className="text-xl font-bold mb-2">Project description</h2>
-						<p>{formInputs.projectDescription}</p>
+						<p className="pl-4 sm:pl-10 xl:pl-60">{formInputs.projectDescription}</p>
 					</div>
 					<div className="mb-6">
 						<h2 className="text-xl font-bold mb-2">Creator motivations</h2>
-						<p>{formInputs.creatorMotivations}</p>
+						<p className="pl-4 sm:pl-10 xl:pl-60">{formInputs.creatorMotivations}</p>
 					</div>
 					<div className="mb-6">
 						<h2 className="text-xl font-bold mb-2">Project objectives</h2>
-						<p>{formInputs.projectObjectives}</p>
+						<p className="pl-4 sm:pl-10 xl:pl-60">{formInputs.projectObjectives}</p>
 					</div>
 					<div className="mb-6">
 						<h2 className="text-xl font-bold mb-2">Project online Only</h2>
-						<p>{formInputs.locationOnlineOnly ? "Yes" : "No"}</p>
+						<p className="pl-4 sm:pl-10 xl:pl-60">{formInputs.locationOnlineOnly ? "Yes" : "No"}</p>
 					</div>
 					{!formInputs.locationOnlineOnly && (
 						<>
 							<div className="mb-6">
 								<h2 className="text-xl font-bold mb-2">Country</h2>
-								<p>{formInputs.locationCountry}</p>
+								<p className="pl-4 sm:pl-10 xl:pl-60">{formInputs.locationCountry}</p>
 							</div>
 							<div className="mb-6">
 								<h2 className="text-xl font-bold mb-2">City</h2>
-								<p>{formInputs.locationCity}</p>
+								<p className="capitalize pl-4 sm:pl-10 xl:pl-60">{formInputs.locationCity}</p>
 							</div>
 						</>
-					)}{" "}
+					)}
 					<div className="mb-6">
 						<h2 className="text-xl font-bold mb-2">Project visibility</h2>
-						<p className="capitalize">{formInputs.projectVisibility}</p>
+						<p className="capitalize pl-4 sm:pl-10 xl:pl-60">{formInputs.projectVisibility}</p>
 					</div>
 					<div className="mb-6">
 						<h2 className="text-xl font-bold mb-2">Start date</h2>
-						<p>{formatDate(formInputs.projectStartDate)}</p>
+						<p className="pl-4 sm:pl-10 xl:pl-60">{formatDate(formInputs.projectStartDate)}</p>
 					</div>
 					<div className="mb-6">
 						<h2 className="text-xl font-bold mb-2">Tags</h2>
 						{formInputs.tags.length > 0 ? (
-							<div className="flex flex-wrap gap-2">
+							<div className="pl-4 sm:pl-10 xl:pl-60 flex flex-wrap gap-2">
 								{formInputs.tags.map((tag, index) => (
 									<span key={index} className="flex items-center px-3 pt-0.5 pb-1 mt-1 bg-gray-200 text-gray-800 rounded-full">
 										{tag}
